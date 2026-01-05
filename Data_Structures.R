ad1cf6e5fb49dac2be238411c6ce6d60c7448557
gouthamigorantla <rajesh.malempati94@gmail.com>
2026-01-04 23:42:27 -0600

    data structures

---
+++ Data_Structures.R
@@ -0,0 +1,39 @@
+#defining a vector, List, Data frame, Array, Matrix
+vector_example<-c(10,20,30,40,50)
+vector_example
+class(vector_example)
+is.vector(vector_example)
+#converting vector into list
+list_example<-as.list(vector_example)
+class(list_example)
+list_example
+is.list(list_example)
+is.vector(list_example)
+is.array(list_example)
+
+#defining a vector
+#accessing(indexing) a vector
+#updating a vector
+#deleting a value in a vector
+#converting a vector into another data structure
+#checking a vector
+#storing a multiple data types into a vector
+#checking a missing value in a vector
+#sorting a vector
+#replacing a missing value in a vector
+#naming a vector
+
+
+multivector<-c(10,"hello")
+class(multivector)
+multivector_2<-c(10,TRUE)
+class(multivector_2)
+Multivector_3<-c(TRUE, "hi")
+class(Multivector_3)
+multivector_4<-c(20,"Hello", TRUE)
+class(multivector_4)
+
+#logical<integer<numeric< charecter (in size)
+
+
+

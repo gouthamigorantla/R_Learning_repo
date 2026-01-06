63908a09ab08c9f89c52bfe518717d035c917fc8
gouthamigorantla <rajesh.malempati94@gmail.com>
2026-01-05 23:36:39 -0600

    data structures and vector defining,accessing

--- Data_Structures.R
+++ Data_Structures.R
@@ -35,5 +35,65 @@ class(multivector_4)
 
 #logical<integer<numeric< charecter (in size)
 
+ratio<-1:7
+ratio
+seq_example<-seq(0,10,by=3)
+seq_example
+rep_example<-rep(1:7, 2)
+rep_example
+mean_result<-mean(rep_example)
+mean_result
+max_value<-max(rep_example)
+max_value
+min_value<-min(multivector_4)
+min_value
+max_value1<-max(multivector)
+max_value1
+
+#accessing a vector
+rep_example
+length(rep_example) #to find the length of a vector
+#sum of 8th value and 6th value and 14th value from the rep_example vector
+value<-rep_example[8] #index position starts with 1
+value
+value1<-rep_example[6]
+value1
+value2<-rep_example[14]
+value2
+value+value1+value2 #or 
+result1<-rep_example[8] + rep_example[6]+rep_example[14]
+result1
+
+char_vector<-c("shiny","R","java","c#","spring", "docker")
+num_vector<-c(50,20,70,10,30,90)
+mixed_vector<-c(10,29.56,22L,TRUE,"Hello",49,FALSE,"Hi")
+missing_vector<-c(44,55," ",66,NA,77)
+rep_example
+math_vector<-c(math=45,social=67,name="gouthami") 
+math_vector
+math_vector["math"]
+math_vector[1]
+length(mixed_vector)
+mixed_vector[2:5]
+char_vector[3]
+char_vector[-4]
+
+#sorting a vector
+sort(num_vector, decreasing = TRUE) #sorting only values
+sort(char_vector)
+order(char_vector) #order index positions
+order(num_vector)
+#O/P:---hi gouthami 90 R docker hello shiny
+paste0("Hello", mixed_vector[8]," ",math_vector["name"]," ",num_vector[6]+num_vector[4]," ",char_vector[2]," ",char_vector[6],mixed_vector[5],char_vector[1])
+max(num_vector)
+unique(rep_example)
+length(rep_example)
+length(unique(rep_example))
+#find out the second highest number of rep_example
+unique(min(rep_example)+1)
+
+
+
+
 
 

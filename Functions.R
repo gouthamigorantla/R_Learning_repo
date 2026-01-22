62a4b87163ccd2c4991ca2f39c728c8cdada02c4
gouthamigorantla <rajesh.malempati94@gmail.com>
2026-01-21 22:52:21 -0600

    Functions

--- Functions.R
+++ Functions.R
@@ -13,7 +13,33 @@ addition_fun<- function(a,b){ #function with arguments
   c<-a+b
   c
 }
-addition_fun(50,50)
+addition_fun(b=60,a=70)
+
+addition_fun<- function(a=10,b){ #function with arguments
+  c<-a+b
+  c
+}
+addition_fun(b=30)
+addition_fun(a=30,b=40)
+addition_fun(30,40)
+addition_fun(b=40,a=30)
+
+name_fun<-function(name_input){
+  print("Hello mounika")
+  print(paste("Hello",name_input))
+  print(name_input)
+}
+name_fun(name_input="nagadeep")
+
+dataset_fun<-function(dataset){
+  print("hello")
+  print(dim(dataset))
+  print("result")
+  print(head(dataset))
+}
+dataset_fun(dataset = 4)
+
+
 
 #Creating a table with using functions
 #2*1=2

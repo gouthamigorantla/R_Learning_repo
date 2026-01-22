dcb761b60011ca55878cf96159a092f997ce16f0
gouthamigorantla <rajesh.malempati94@gmail.com>
2026-01-21 23:24:37 -0600

    control statements

---
+++ Control_Stattements.R
@@ -0,0 +1,51 @@
+#control statements
+#Looping Statements
+#loop Control Statements
+
+x<-30
+if(x<5){
+  print("X number is smaller")
+} else{
+  print("X is bigger number")
+}
+
+x<-20
+y<-30
+if(x==y){
+  print("X is equals to Y")
+} else{
+  print("Y is not equals to X")
+}
+
+if(x!=y){
+  print("X is not equals to Y")
+} else{
+  print("Y is  equals to X")
+}
+
+if(x>y){
+  print("X is grater than to Y")
+} else{
+  print("Y is not equals to X")
+}
+
+if(x<y){
+  print("X is equals to Y")
+} else{
+  print("Y is not equals to X")
+}
+
+#else if, if condition
+#take the number check if it is equal to 90 or greater than print statement grade A, else if condition greater than or equal to 75 print grade B,  else then print grade C. 
+mark<-40
+if(mark>=90){
+  print("Grade A")
+} else if(mark>=75){
+  print("Grade B")
+} else{
+  print("Grade C")
+}
+
+
+
+
